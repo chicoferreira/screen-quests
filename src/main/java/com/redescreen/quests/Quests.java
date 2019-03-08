aa267ba19179dc19084873b608a4ba51af008e25
Francisco Ferreira <extremeartz23@gmail.com>
2019-03-08 23:38:54 +0000

    first commit

---
+++ src/main/java/com/redescreen/quests/Quests.java
@@ -0,0 +1,14 @@
+package com.redescreen.quests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Quests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
